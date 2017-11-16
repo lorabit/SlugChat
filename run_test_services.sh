385543f21b60b8eb0964fdcbf5be6418e98907a5
lorabit <lorabit@126.com>
2017-11-16 01:02:08 -0800

    add shell scripts for runing all services

---
+++ run_test_services.sh
@@ -0,0 +1,20 @@
+mkdir ~/slugchat
+
+kill `cat ~/slugchat/mobile.pid`
+kill `cat ~/slugchat/api.pid`
+
+
+bazel build main/java/slugchat/mobile/service:bin
+bazel-bin/main/java/slugchat/mobile/service/bin \
+  --port 50051 \
+  --log /tmp/slugchat.log \
+  --db.url "jdbc:mysql://slugchat-test.cqpr0kxypucj.us-west-2.rds.amazonaws.com/slugchat?user=root&password=password&useUnicode=yes&characterEncoding=UTF-8" \
+  --dialogflow.apikey 360f730cc1034b19864a1cae700ff632 \
+  &
+echo $! > ~/slugchat/mobile.pid
+
+bazel build main/java/slugchat/api:bin
+bazel-bin/main/java/slugchat/api/bin \
+  --server.port=8080 \
+  &
+echo $! > ~/slugchat/api.pid
---
+++ shutdown.sh
@@ -0,0 +1,2 @@
+kill `cat ~/slugchat/mobile.pid`
+kill `cat ~/slugchat/api.pid`
