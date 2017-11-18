a107e3e2cb631bb96b58d23e373f2d8017496855
lorabit <lorabit@126.com>
2017-11-18 00:43:10 -0800

    auto reconnect = true

--- run_test_services.sh
+++ run_test_services.sh
@@ -9,7 +9,7 @@ bazel build main/java/slugchat/mobile/service:bin
 bazel-bin/main/java/slugchat/mobile/service/bin \
   --port 50051 \
   --log /tmp/slugchat.log \
-  --db.url "jdbc:mysql://slugchat-test.cqpr0kxypucj.us-west-2.rds.amazonaws.com/slugchat?user=root&password=password&useUnicode=yes&characterEncoding=UTF-8" \
+  --db.url "jdbc:mysql://slugchat-test.cqpr0kxypucj.us-west-2.rds.amazonaws.com/slugchat?user=root&password=password&useUnicode=yes&characterEncoding=UTF-8&autoReconnect=true" \
   --dialogflow.apikey 360f730cc1034b19864a1cae700ff632 \
   >> ~/slugchat/mobile.out 2>>~/slugchat/mobile.err \
   &
